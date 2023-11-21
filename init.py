afeb4077b766012679e25ed2c177cfff9c2cdead
Zachary Hollmann <zachary.hollmann@gmail.com>
2023-11-21 14:29:44 -0500

    Modified error handleing

--- init.py
+++ init.py
@@ -18,22 +18,24 @@ try:
                 first_name TEXT NOT NULL,
                 last_name TEXT NOT NULL,
                 email TEXT UNIQUE NOT NULL,
-                enrollment_date DATE)
-                """)
+                enrollment_date DATE)""")
+    
     # Make the changes to the database persistent
     conn.commit()
-    print('Success!')
+    print('Student table created successfully!')
+
+    # Execute a command: insert initial data into students table
+    cur.execute("""INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+    ('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+    ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+    ('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02')""")
+
+    # Make the changes to the database persistent
+    conn.commit()
+    print("Student table populated successfully!")
 
 except Exception as error:
     print("ERROR:", error)
-
-# Execute a command: insert initial data into students table
-cur.execute("""INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
-('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
-('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
-('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02')""")
-# Make the changes to the database persistent
-conn.commit()
 
 # Close cursor and communication with the database
 cur.close()
